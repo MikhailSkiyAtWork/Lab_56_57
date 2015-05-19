97d0af7d7867768fb93eea0178957b1ba475a936
Mikhail Valuyskiy <mvaluyskiy@weezlabs.com>
2015-05-19 17:07:44 +0300

    Created new Activity with buttons which helps switch between lab56 and lab57

---
+++ app/src/main/java/com/example/admin/lab_56_57/ActivityForLab56Fragment.java
@@ -0,0 +1,23 @@
+package com.example.admin.lab_56_57;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActivityForLab56Fragment extends Fragment {
+
+    public ActivityForLab56Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activity_for_lab56, container, false);
+    }
+}
